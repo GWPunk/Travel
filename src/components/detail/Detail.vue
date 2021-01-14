e86c6c45dfdbd552d5724dad3351d1485785e1a8
Govin <1124960732@qq.com>
2021-01-14 14:36:36 +0800

    finish detail list

--- src/components/detail/Detail.vue
+++ src/components/detail/Detail.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-   <banner></banner>
+   <detail-banner></detail-banner>
    <detail-header></detail-header>
+   <detail-list :list="categoryList"></detail-list>
    <div class="content"></div>
   </div>
 </template>
 
 <script>
-import Banner from './Banner'
+import DetailBanner from './DetailBanner'
 import DetailHeader from './DetailHeader'
+import DetailList from './DetailList'
 export default {
   name: 'Detail',
+  data () {
+    return {
+      categoryList: [{
+        "title": "成人票",
+        "children": [{
+          "title": "成人三馆联票",
+          "children": [{
+            "title": "成人三馆联票 - 某一连锁店销售"
+          }]
+        },{
+          "title": "成人五馆联票"
+        }]
+      }, {
+        "title": "学生票"
+      }, {
+        "title": "儿童票"
+      }, {
+        "title": "特惠票"
+      }]
+    }
+  },
   components: {
-    Banner,
+    DetailBanner,
     DetailHeader,
+    DetailList,
   }
 }
 
