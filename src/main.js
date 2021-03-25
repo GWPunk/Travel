60ed1dc5e9433bbabaa8af4e830b08d34167d5f4
gao <1124960732@qq.com>
2021-03-25 14:10:40 +0800

    fastclick solve

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import fastClick from 'fastclick' 
+import fastClick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 // import 'default-passive-events'
 import 'babel-polyfill'
@@ -13,7 +13,11 @@ import 'swiper/css/swiper.css'
 
 Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
-fastClick.attach(document.body)
+
+document.addEventListener('DOMContentLoaded', function () {
+  FastClick.attach(document.body);
+}, false);
+
 new Vue({
   router,
   store,
